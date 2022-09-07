57a68dc3a1b76c830b41b54c9177ec285c944525
bertrand Clech <bertrand.clech@cogenio.com>
2022-09-07 09:59:32 +0200

    step 1 to 3 rooting, ...

--- about-the-universe/src/app/app-routing.module.ts
+++ about-the-universe/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./home-page/home-page.component";
+import {IndexShowPlanetsComponent} from "./index-show-planets/index-show-planets.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home-page', component: HomePageComponent},
+  {path: 'index-show-planets', component: IndexShowPlanetsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
